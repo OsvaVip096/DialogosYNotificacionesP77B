f84af63f7c14984978374b5958696f51dc574c53
OsvaX <s15120062@alumnos.itsur.edu.mx>
2019-10-25 13:43:23 -0500

    Cambio menor en Intent.

--- app/src/main/java/com/osvaldovillalobosperez/dialogosynotificacionesp77b/MainActivity.java
+++ app/src/main/java/com/osvaldovillalobosperez/dialogosynotificacionesp77b/MainActivity.java
@@ -149,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnNS_click(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent("net.ivanvega.audioenandroidcurso.CAPTURARAUDIO");
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
